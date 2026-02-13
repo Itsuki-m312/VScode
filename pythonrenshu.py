e7e43a8bdbcf5f3bd751c465ba04af0f1cc9f8cd
Itsuki-m312 <itsuim53im@gmail.com>
2026-02-14 02:15:44 +0900

    test

--- pythonrenshu.py
+++ pythonrenshu.py
@@ -1,2 +1,18 @@
-print("konbanha")
+
 print("aiueo")
+3**2
+3/2
+0.1 + 0.2
+#python.jp、変数
+(100 * 5) + (40 * 8) + (80 * 5) + (60 * 9) + (90 * 20) + (110 * 10)
+
+momo = 100 * 5
+mikan = 40 * 8
+nashi = 80 * 5
+kiwi = 60 * 9
+suika = 90 * 20
+kaki = 110 * 10
+
+print(momo + mikan + nashi + kiwi + suika + kaki)
+#対話モードになってて全然実行できなかった。>>>がターミナルに表示されていたら
+#ターミナルにexit()を入力して解決！エラー例：File "<stdin>", line 1 ca
