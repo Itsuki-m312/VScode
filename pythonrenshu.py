4dcbc0ae8b8a41090788857ab2d22957b79c9689
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-02-14 13:15:59 +0900

    関数少し

--- pythonrenshu.py
+++ pythonrenshu.py
@@ -17,6 +17,24 @@ suika = 90 * 20
 kaki = 110 * 10
 
 print(momo + mikan + nashi + kiwi + suika + kaki)
-#対話モードになってて全然実行できなかった。>>>がターミナルに表示されていたら
-#ターミナルにexit()を入力して解決！エラー例：File "<stdin>", line 1 
+#対話モードになっているとき（>>>がターミナルに表示されていたら）
+#ターミナルにexit()を入力して解決-エラー例：File "<stdin>", line 1 
 #emailをgithub標準のメアドにする0
+print(abs(-123))
+minus_value = 100 * -1
+print(abs(minus_value))
+#absは絶対値を表示する
+
+print(round(1.124 , 2))
+#roundは有効桁数を指定して、その桁数にした値を表示するround(桁数を変更したい数字,有効桁数)
+
+print(min(3, 4, 90, 5))
+print(max(4, 5, 3, 9, 20))
+#minは最小の数、maxは最大の数を戻り値として返す　好きなだけ引数を指定できる
+
+print(int(10.5))
+print(int(5 / 2))
+
+print(float(30))
+#intはfloat型（小数）の値を整数に変換する（小数点以下を切り捨てる）
+#floatは整数の値をfloat型に変換する
