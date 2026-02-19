c1b5ed168b3ea6c4440ae34b68cca1878face131
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-02-19 17:52:38 +0900

    5日目

--- mealdec_cbu.py
+++ mealdec_cbu.py
@@ -1,6 +1,13 @@
 import random
 
-foods = ["そば","焼きそば", "パン", "チャーハン", "パスタ", "冷食", "ラーメン"]
+select = input("麺、ご飯、パンから選んでください")
+if select == "麺":
+    foods = ["そば", "焼きそば", "うどん", "パスタ",]
+elif select == "ご飯":
+    foods = ["ドリア","チャーハン","弁当",]
+elif select == "パン":
+    foods = ["お惣菜パン","ホットドッグ","ハンバーガー","サンドイッチ",]
+
 
 choice = random.choice(foods)
 
