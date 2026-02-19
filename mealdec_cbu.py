a0d31c91bb4a53f8fcf1449da5038e1b74fba792
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-02-19 17:54:14 +0900

    種類追加

--- mealdec_cbu.py
+++ mealdec_cbu.py
@@ -2,7 +2,7 @@ import random
 
 select = input("麺、ご飯、パンから選んでください")
 if select == "麺":
-    foods = ["そば", "焼きそば", "うどん", "パスタ",]
+    foods = ["そば", "焼きそば", "うどん", "パスタ","ラーメン"]
 elif select == "ご飯":
     foods = ["ドリア","チャーハン","弁当",]
 elif select == "パン":
