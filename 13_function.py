8440dfe06614ef7203a1ce0839c1d41061057667
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-02-25 23:04:55 +0900

    test

---
+++ 13_function.py
@@ -0,0 +1,14 @@
+mikan = input("みかんの個数")
+momo = input("桃の個数")
+mikan_number = int(mikan)
+momo_number = int(momo)
+total_mikan = mikan_number * 40
+total_momo = momo_number * 100
+total = total_mikan + total_momo
+print("みかん",total_mikan,"円","もも",total_momo,"円","合計",total,"円")
+
+def fruit_price(number_of_mikan,number_of_momo):#defで関数を作ることができる（関数を定義する)
+    
+
+
+
