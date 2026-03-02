7b4c728228ec092ab4b9bb036e5710789cdf474f
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-03-02 23:31:01 +0900

    len

---
+++ 15_list-loop.py
@@ -0,0 +1,13 @@
+cases = [109,104,98,140,123,83,120,]
+
+index = 0
+
+total = 0
+
+while index < len(cases): #len()はリストの要素数を返す
+    
+    total =total + cases[index]
+    index = index + 1
+
+print(total)
+
