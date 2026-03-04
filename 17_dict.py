78f430eadee157bb5b2a2cedbd93690abe559af4
Itsuki-m312 <261241799+Itsuki-m312@users.noreply.github.com>
2026-03-04 22:02:24 +0900

    dict

--- 17_dict.py
+++ 17_dict.py
@@ -3,3 +3,17 @@ eng = {"apple":"りんご","peach":"もも","cow":"うし"}
 print(eng)
 
 print(eng["cow"])#辞書オブジェクト[キー]で参照できる
+
+eng["dog"] = "犬" #辞書オブジェクト[キー] = 値　で辞書オブジェクトに要素を追加することができる
+
+print(eng["dog"])
+
+eng["dog"] = "いぬ" #置き換え
+
+print(eng["dog"])
+
+del eng["dog"] #要素の削除
+
+print(eng)
+
+print(len(eng)) #要素数
